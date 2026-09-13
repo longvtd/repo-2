023cf3e467e9223609340b7b87d65b72a98833c6
Vo Tran Dinh Long <46593195+longvtd@users.noreply.github.com>
2026-09-13 20:04:43 +0700

    fix: test to commit createOrder

--- src/main/java/com/acme/b/api/OrderController.java
+++ src/main/java/com/acme/b/api/OrderController.java
@@ -6,7 +6,10 @@ import java.math.BigDecimal;
 @RequestMapping("/api/partner-orders")
 public class OrderController {
  private final OrderService service; public OrderController(OrderService service){this.service=service;}
- @PostMapping public long createOrder(@RequestParam String customerId,@RequestParam BigDecimal amount){return service.createOrder(customerId,amount);}
+ @PostMapping public long createOrder(@RequestParam String customerId,@RequestParam BigDecimal amount){
+     //test to commit createOrder
+     return service.createOrder(customerId,amount);
+ }
  @GetMapping("/health") public String health(){
      //test change controller api health
      return "B-OK";
